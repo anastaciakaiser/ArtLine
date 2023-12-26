5f5a24c015ee40c618eebe78fabd2b4d56831255
Anastasia Kaiser <kaiseranastay@gmail.com>
2023-12-26 20:48:30 +0600

    added cors  configurations for api

--- ArtLineServer/src/main/java/com/vortex/ArtLineServer/config/SecurityConfiguration.java
+++ ArtLineServer/src/main/java/com/vortex/ArtLineServer/config/SecurityConfiguration.java
@@ -12,6 +12,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import static com.vortex.ArtLineServer.user.Permission.*;
 import static com.vortex.ArtLineServer.user.Role.*;
@@ -30,6 +33,8 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors()
+                .and()
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
@@ -69,6 +74,19 @@ public class SecurityConfiguration {
 
         return http.build();
 
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:3000"); // Замените на фактический адрес вашего фронтенда
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
